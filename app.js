beba01a3ca3259298cf389aefe4461ee91e9e6b6
sahar feferovich <sahar.fefer@gmail.com>
2020-07-02 13:33:21 +0300

    connected!

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const apartmentsRouter = require('./routes/apartments');
 const usersRouter = require('./routes/users');
@@ -10,6 +11,7 @@ const citiesRouter = require('./routes/cities');
 
 const app = express();
 
+app.use(cors({origin: 'http://localhost:3000', credentials: true}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
